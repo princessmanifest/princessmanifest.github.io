c17e8cff291a71e5a306cd9fbf5559dfacaccb67
cindyjiang23 <cindyjia@usc.edu>
2025-04-30 12:21:18 -0700

    Fix JSON paths to use /data folder

--- visualizations/cindy/main.js
+++ visualizations/cindy/main.js
@@ -2,10 +2,10 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import { WordCloud } from './wordcloud.js';  
 
 const regions = [
-    { id: "northeast", file: "northeast_wordcloud_d3.json", title: "Northeast" },
-    { id: "midwest",   file: "midwest_wordcloud_d3.json",   title: "Midwest" },
-    { id: "south",     file: "south_wordcloud_d3.json",     title: "South" },
-    { id: "west",      file: "west_wordcloud_d3.json",      title: "West" }
+    { id: "northeast", file: "data/northeast_wordcloud_d3.json", title: "Northeast" },
+    { id: "midwest",   file: "data/midwest_wordcloud_d3.json",   title: "Midwest" },
+    { id: "south",     file: "data/south_wordcloud_d3.json",     title: "South" },
+    { id: "west",      file: "data/west_wordcloud_d3.json",      title: "West" }
   ];
   
 

505ee539214d735e3ce9368deca06cd3d2d8ae36
yhuang27 <yhuang27@usc.edu>
2025-04-29 18:06:16 -0700

    Update script.js

--- visualizations/daisy/script.js
+++ visualizations/daisy/script.js
@@ -1,45 +1,54 @@
-// Set up dimensions and margins
+// Dimensions and SVG setup
 const margin = { top: 30, right: 30, bottom: 40, left: 60 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
 
-// Create SVG canvas
 const svg = d3.select("#chart")
   .append("svg")
   .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom])
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
 
-// Load data
 d3.json("./data/apparition_yearly.json").then(data => {
   const keys = Object.keys(data[0]).filter(k => k !== "Year");
 
-  // Set up x and y scales
+  const stackedData = d3.stack().keys(keys)(data);
+
   const x = d3.scaleLinear()
     .domain(d3.extent(data, d => d.Year))
     .range([0, width]);
 
   const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d => {
-      return keys.reduce((sum, key) => sum + d[key], 0);
-    })])
-    .nice()
+    .domain([0, d3.max(stackedData[stackedData.length - 1], d => d[1])])
     .range([height, 0]);
 
   const color = d3.scaleOrdinal()
     .domain(keys)
     .range(d3.schemeCategory10);
 
-  const stackedData = d3.stack()
-    .keys(keys)(data);
-
-  // Area generator
   const area = d3.area()
     .x(d => x(d.data.Year))
     .y0(d => y(d[0]))
     .y1(d => y(d[1]));
 
-  // Draw stacked areas
-  svg.selectAll(".layer")
-    .data(st
+  svg.selectAll("path")
+    .data(stackedData)
+    .join("path")
+    .attr("fill", d => color(d.key))
+    .attr("d", area)
+    .append("title")
+    .text(d => d.key);
 
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x).tickFormat(d3.format("d")))
+    .selectAll("text")
+    .style("fill", "white");
+
+  svg.append("g")
+    .call(d3.axisLeft(y))
+    .selectAll("text")
+    .style("fill", "white");
+
+  svg.selectAll(".domain, .tick line").attr("stroke", "#777");
+});

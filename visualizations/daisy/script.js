f1f8f330b6c23ca9a7f749361c1b8ba06bd382e0
yhuang27 <yhuang27@usc.edu>
2025-04-30 00:00:46 -0700

    Update script.js 4

--- visualizations/daisy/script.js
+++ visualizations/daisy/script.js
@@ -1,11 +1,11 @@
-const margin = { top: 30, right: 150, bottom: 50, left: 70 },
-      width = 1400 - margin.left - margin.right,
-      height = 700 - margin.top - margin.bottom;
+const margin = { top: 50, right: 180, bottom: 60, left: 90 },
+      width = 1800 - margin.left - margin.right,
+      height = 900 - margin.top - margin.bottom;
 
 const svg = d3.select("#chart")
   .append("svg")
-  .attr("viewBox", [0, 0, width + margin.left + margin.right + 150, height + margin.top + margin.bottom])
-  .style("max-width", "100%")
+  .attr("viewBox", [0, 0, width + margin.left + margin.right + 200, height + margin.top + margin.bottom])
+  .style("width", "100%")
   .style("height", "auto")
   .append("g")
   .attr("transform", `translate(${margin.left},${margin.top})`);
@@ -17,9 +17,10 @@ const tooltip = d3.select("#chart")
   .style("background", "white")
   .style("color", "#111")
   .style("border", "1px solid #ccc")
-  .style("padding", "6px")
-  .style("border-radius", "4px")
-  .style("font-size", "12px");
+  .style("padding", "8px")
+  .style("border-radius", "5px")
+  .style("font-size", "14px")
+  .style("pointer-events", "none");
 
 d3.json("./data/apparition_yearly.json").then(data => {
   const keys = Object.keys(data[0]).filter(k => k !== "Year");
@@ -37,7 +38,7 @@ d3.json("./data/apparition_yearly.json").then(data => {
 
   const color = d3.scaleOrdinal()
     .domain(keys)
-    .range(d3.schemeCategory10);
+    .range(d3.schemeTableau10);
 
   const area = d3.area()
     .curve(d3.curveBasis)
@@ -53,12 +54,11 @@ d3.json("./data/apparition_yearly.json").then(data => {
     .attr("d", area)
     .style("opacity", 0.85)
     .on("mouseover", function(event, d) {
-      tooltip.style("visibility", "visible")
-        .html(`<strong>${d.key}</strong>`);
+      tooltip.style("visibility", "visible").html(`<strong>${d.key}</strong>`);
     })
     .on("mousemove", function(event) {
       tooltip
-        .style("top", (event.pageY - 10) + "px")
+        .style("top", (event.pageY + 10) + "px")
         .style("left", (event.pageX + 10) + "px");
     })
     .on("mouseout", () => tooltip.style("visibility", "hidden"));
@@ -67,24 +67,26 @@ d3.json("./data/apparition_yearly.json").then(data => {
     .attr("transform", `translate(0,${height})`)
     .call(d3.axisBottom(x).tickFormat(d3.format("d")))
     .selectAll("text")
-    .style("fill", "white");
+    .style("fill", "white")
+    .style("font-size", "14px");
 
   svg.append("g")
     .call(d3.axisLeft(y))
     .selectAll("text")
-    .style("fill", "white");
+    .style("fill", "white")
+    .style("font-size", "14px");
 
   svg.selectAll(".domain, .tick line").attr("stroke", "#777");
 
   const legend = svg.append("g")
-    .attr("transform", `translate(${width + 20}, 0)`)
+    .attr("transform", `translate(${width + 30}, 0)`)
     .attr("class", "legend");
 
   keys.forEach((key, i) => {
     const safeKey = key.replace(/\\s+/g, '-');
 
     const row = legend.append("g")
-      .attr("transform", `translate(0, ${i * 20})`)
+      .attr("transform", `translate(0, ${i * 25})`)
       .style("cursor", "pointer")
       .on("click", function () {
         if (activeKey === key) {
@@ -98,15 +100,15 @@ d3.json("./data/apparition_yearly.json").then(data => {
       });
 
     row.append("rect")
-      .attr("width", 12)
-      .attr("height", 12)
+      .attr("width", 14)
+      .attr("height", 14)
       .attr("fill", color(key));
 
     row.append("text")
-      .attr("x", 16)
-      .attr("y", 10)
+      .attr("x", 20)
+      .attr("y", 12)
       .text(key)
       .style("fill", "white")
-      .style("font-size", "12px");
+      .style("font-size", "14px");
   });
 });

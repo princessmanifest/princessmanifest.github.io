8a88eedd182758fe59c0d5b98e46f703692d1bff
elixier2 <166572779+elixier2@users.noreply.github.com>
2025-05-01 10:57:02 -0700

    Update script.js

--- visualizations/elisa/script.js
+++ visualizations/elisa/script.js
@@ -1,10 +1,15 @@
+// Load Plotly
 import { Plotly } from "https://cdn.plot.ly/plotly-2.26.0.min.js";
 
-// Fetch the chart JSON and render it
+// Load the chart JSON and render it
 fetch("haunted_objects_chart.json")
   .then(response => response.json())
   .then(fig => {
+    // Ensure text and layout matches dark theme
+    fig.layout.paper_bgcolor = 'rgba(0,0,0,0)';
+    fig.layout.plot_bgcolor = 'rgba(0,0,0,0)';
+    fig.layout.font = { color: "white" };
+
     Plotly.newPlot("chart", fig.data, fig.layout, { responsive: true });
   })
   .catch(error => console.error("Error loading chart JSON:", error));
-

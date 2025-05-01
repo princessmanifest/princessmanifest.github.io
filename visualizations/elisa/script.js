0d96eec008b8fc5d2c262780f1c94ae6d74b48cf
elixier2 <166572779+elixier2@users.noreply.github.com>
2025-05-01 08:17:23 -0700

    Update script.js

--- visualizations/elisa/script.js
+++ visualizations/elisa/script.js
@@ -1,1 +1,10 @@
+import { Plotly } from "https://cdn.plot.ly/plotly-2.26.0.min.js";
+
+// Fetch the chart JSON and render it
+fetch("haunted_objects_chart.json")
+  .then(response => response.json())
+  .then(fig => {
+    Plotly.newPlot("chart", fig.data, fig.layout, { responsive: true });
+  })
+  .catch(error => console.error("Error loading chart JSON:", error));
 

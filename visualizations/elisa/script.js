b8c6907524997861cad4b343529424ae8b2acf79
elixier2 <166572779+elixier2@users.noreply.github.com>
2025-05-01 18:41:36 -0700

    Update script.js

--- visualizations/elisa/script.js
+++ visualizations/elisa/script.js
@@ -6,12 +6,10 @@ fetch('haunted_objects_chart.json')
     return response.json();
   })
   .then(data => {
-    Plotly.newPlot('bar-chart', data.data, data.layout, {responsive: true});
+    Plotly.newPlot('bar-chart', data.data, data.layout, { responsive: true });
   })
   .catch(error => {
     console.error('Error loading chart:', error);
     const chartContainer = document.getElementById('bar-chart');
     chartContainer.innerHTML = `<p style="color:white">Error loading chart: ${error.message}<br>Please check the JSON file path or format.</p>`;
   });
-
-
